31194588d70ecd0dc1b4f8f3ca0860268ac7a305
Matthew Ballance <matt.ballance@gmail.com>
2020-02-29 13:45:59 -0500

    Fully-specify module to check
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- scripts/run_pylint.py
+++ scripts/run_pylint.py
@@ -7,11 +7,12 @@ from pylint import run_pylint
 scripts_dir = os.path.dirname(os.path.abspath(__file__))
 vsc_dir = os.path.dirname(scripts_dir)
 
+
 sys.argv = [sys.argv[0]]
 
 sys.argv.append("-E")
 sys.argv.append("--extension-pkg-whitelist=pyboolector")
-sys.argv.append(os.path.join(vsc_dir, "src"))
+sys.argv.append(os.path.join(vsc_dir, "src", "vsc"))
 
 for a in sys.argv:
     print("Argument: " + str(a))
