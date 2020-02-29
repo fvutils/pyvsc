0e4c00d67a0098213c7c33a7cc2f9ee76ee076ea
Matthew Ballance <matt.ballance@gmail.com>
2020-02-28 22:04:52 -0500

    add basic CI infrastructure
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 
 import os
+import ivpm
 from setuptools import setup
 
 setup(
-  name = "py-vsc",
+  name="py-vsc",
+  version=ivpm.get_pkg_version(__file__),
   packages=['vsc'],
-  package_dir = {'' : 'src'},
-  author = "Matthew Ballance",
-  author_email = "matt.ballance@gmail.com",
-  description = ("py-vsc (Verification Stimulus and Coverage) is a Python package for generating randomized stimulus and defining and collecting functional coverage."),
-  license = "Apache 2.0",
-  keywords = ["SystemVerilog", "Verilog", "RTL", "Coverage"],
+  package_dir={'' : 'src'},
+  author="Matthew Ballance",
+  author_email="matt.ballance@gmail.com",
+  description=("py-vsc (Verification Stimulus and Coverage) is a Python package for generating randomized stimulus and defining and collecting functional coverage."),
+  license="Apache 2.0",
+  keywords = ["Python", "Functional Verification", "Constraints", "Coverage"],
   url = "https://github.com/fvutils/py-vsc",
   entry_points={
     'console_scripts': [
@@ -19,8 +21,10 @@ setup(
   },
   setup_requires=[
     'setuptools_scm',
+    'ivpm',
   ],
   install_requires=[
+    'pyboolector',
   ],
 )
 
