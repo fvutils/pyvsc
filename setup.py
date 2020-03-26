56090aad0fccafb17b86b0c7b303b7094abba307
Matthew Ballance <matt.ballance@gmail.com>
2020-03-25 20:49:27 -0700

    correct names
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import ivpm
 from setuptools import setup
 
 setup(
-  name="py-vsc",
+  name="pyvsc",
   version=ivpm.get_pkg_version(__file__),
   packages=['vsc'],
   package_dir={'' : 'src'},
   author="Matthew Ballance",
   author_email="matt.ballance@gmail.com",
-  description=("py-vsc (Verification Stimulus and Coverage) is a Python package for generating randomized stimulus and defining and collecting functional coverage."),
+  description=("pyvsc (Verification Stimulus and Coverage) is a Python package for generating randomized stimulus and defining and collecting functional coverage."),
   license="Apache 2.0",
   keywords = ["Python", "Functional Verification", "Constraints", "Coverage"],
-  url = "https://github.com/fvutils/py-vsc",
+  url = "https://github.com/fvutils/pyvsc",
   entry_points={
     'console_scripts': [
       'vsc = vsc.__main__:main'
