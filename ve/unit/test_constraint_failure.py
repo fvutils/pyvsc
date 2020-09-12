df34f1f942649278e2afc7eec0c1d10f5b692bae
Matthew Ballance <matt.ballance@gmail.com>
2020-09-11 22:47:55 -0400

    Add simple solve-failure diagnostics
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ ve/unit/test_constraint_failure.py
@@ -0,0 +1,35 @@
+'''
+Created on Sep 10, 2020
+
+@author: ballance
+'''
+
+import vsc
+from vsc_test_case import VscTestCase
+from vsc.model.solve_failure import SolveFailure
+
+class TestConstraintFailure(VscTestCase):
+    
+    def test_smoke(self):
+        
+        @vsc.randobj
+        class my_c(object):
+            
+            def __init__(self):
+                self.a = vsc.rand_uint8_t()
+                self.b = vsc.rand_uint8_t()
+                
+            @vsc.constraint
+            def ab_c(self):
+                self.a > 5
+                self.b < 5
+                self.a < self.b
+                
+        it = my_c()
+
+        try:        
+            it.randomize()
+            self.fail("no exception thrown")
+        except SolveFailure as e:
+            pass
+        
