f8ca5a1fe2df14f6d3cb8f2c662f219103a8cb10
Matthew Ballance <matt.ballance@gmail.com>
2022-09-06 08:28:41 -0700

    Updates for pyrct
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/vsc2/impl/__init__.py
+++ src/vsc2/impl/__init__.py
@@ -3,4 +3,5 @@ from .ctor import Ctor
 from .field_ref_impl import FieldRefImpl
 from .randclass_decorator_impl import RandClassDecoratorImpl
 from .rand_state import RandState
+from .typeinfo_vsc import TypeInfoVsc
 from .typeinfo_randclass import TypeInfoRandClass
--- src/vsc2/impl/field_enum_impl.py
+++ src/vsc2/impl/field_enum_impl.py
@@ -17,14 +17,16 @@ class FieldEnumImpl(FieldBaseImpl):
         ctxt : Context = Ctor.inst().ctxt()
         pass
     
-    def get_val(self):
-        val = self._modelinfo._lib_obj.val().val_i()
+    def get_val(self, lib_obj_p):
+        field = lib_obj_p.getField(self._modelinfo._idx)
+        val = field.val().val_i()
         val_e = self._modelinfo._typeinfo._e_info.v2e_m[val]
         return val_e
     
-    def set_val(self, v):
+    def set_val(self, lib_obj_p, v):
+        field = lib_obj_p.getField(self._modelinfo._idx)
         val = self._modelinfo._typeinfo._e_info.e2v_m[v]
-        self._modelinfo._lib_obj.val().set_val_i(val)
+        field.val().set_val_i(val)
     
     @property
     def val(self):
