6b8b9e8dc13477078c850b3408c78f7206dc2f52
Matthew Ballance <matt.ballance@gmail.com>
2024-03-26 17:58:53 -0700

    Manually-implemented clog2 constraint
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

---
+++ ve/unit/test_constraint_functions.py
@@ -0,0 +1,34 @@
+from enum import auto, Enum
+
+from vsc_test_case import VscTestCase
+
+
+class TestConstraintFunctions(VscTestCase):
+    
+    def test_clog2(self):
+        import vsc
+
+        def clog2(lhs, rhs, nbits):
+            with vsc.implies((rhs >= 0) & (rhs < 2)):
+                lhs == 0
+            for i in range(1,nbits):
+                with vsc.implies(((rhs-1) >= (2**(i-1))) & ((rhs-1) < (2**i))):
+                    lhs == i
+
+        @vsc.randobj
+        class C(object):
+
+            def __init__(self):
+                self.a = vsc.rand_bit_t(8)
+                self.b = vsc.rand_bit_t(8)
+
+            @vsc.constraint
+            def clog2_c(self):
+                clog2(self.a, self.b, 8)
+
+        c = C()
+
+        for i in range(256):
+            with c.randomize_with():
+                c.b == i
+            print("a=%d b=%d" % (c.a, c.b))
